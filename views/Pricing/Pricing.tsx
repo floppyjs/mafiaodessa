3fc84c01df3b28c3811b6f126d887de3c5e58e65
YaroslavLazarmafia <odessa.labyrinth@gmail.com>
2026-09-22 01:13:11 +0300

    Update features in Pricing.tsx for offerings

--- views/Pricing/Pricing.tsx
+++ views/Pricing/Pricing.tsx
@@ -12,7 +12,7 @@ const kidsPrices = [
         minimum: 5000,
         image: "/images/kidsMafia/kids-mafia.webp",
         features: [
-            "Мафія",
+            "Реквізит та атмосфера",
             "Професійний ведучий",
         ],
     },
@@ -24,8 +24,8 @@ const kidsPrices = [
         minimum: 7000,
         image: "/images/kidsMafia/kids-bunker.webp",
         features: [
-            "Мафія",
-            "Бункер",
+            "Реквізит та атмосфера",
+            "Мафія + Бункер",
             "Професійний ведучий",
         ],
         popular: true,
@@ -38,8 +38,8 @@ const kidsPrices = [
         minimum: 9000,
         image: "/images/kidsMafia/kids-max.webp",
         features: [
-            "Мафія",
-            "Бункер",
+            "Реквізит та атмосфера",
+            "Мафія + Бункер",
             "Кубки переможцям",
             "Професійний ведучий",
         ],
